56d2ecee183b0109c487f8690d5936e42278ea44
timothy anderson <timothy.anderson@sscinc.com>
2026-01-21 08:45:24 -0500

    change aks nodes vm size to allowed SKU

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "system"
     node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B2s"
   }
 
   identity {
